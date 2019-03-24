ad5506f2bb5c04b31e5cf9ae040fc88f0e69034b
Rayron Rodffer <44624642+Rodffer@users.noreply.github.com>
2019-03-23 23:58:41 -0300

    Update Pacman.gd

--- Pacman.gd
+++ Pacman.gd
@@ -32,6 +32,12 @@ func _process(delta):
 		if delcont.length() < 2:
 			set_position(destdisc * 32 + Vector2(16, 16))
 			posdisc = destdisc
+			
+			
+			if tiles.get_cell(posdisc.x, posdisc.y) == 1: #verifica  pontuacao
+				tiles.set_cell(posdisc.x, posdisc.y, -1) #se tiver elimina celula
+			if tiles.get_cell(posdisc.x, posdisc.y) == 2: #verifica pontuacao grande
+				tiles.set_cell(posdisc.x, posdisc.y, -1) #se tiver elimina celula
 		else:
 			set_position(get_position() + delta * speed * delcont.normalized())
 	else:
@@ -43,17 +49,25 @@ func _process(delta):
 			elif destdiscaux == Vector2(-1, 10):
 				destdiscaux = Vector2(20, 10)
 				set_position(Vector2(21, 10) * 32 + Vector2(16, 16))
-			if tiles.get_cell(destdiscaux.x, destdiscaux.y) != 0:
+			if tiles.get_cell(destdiscaux.x, destdiscaux.y) != 0 and destdiscaux != Vector2(10, 9):  #colisao com as paredes/areaghosts
 				destdisc = destdiscaux
 				destcont = get_position() + 32 * dir
 	
 	dir = Vector2(0, 0)
 	if Input.is_action_pressed("left"):
 		dir = Vector2(-1, 0)
+		set_flip_h(true)
+		set_rotation(0)
 	elif Input.is_action_just_pressed("right"):
 		dir = Vector2(1, 0)
+		set_flip_h(false)
+		set_rotation(0)
 	elif Input.is_action_just_pressed("up"):
 		dir = Vector2(0, -1)
+		set_flip_h(false)
+		set_rotation(80.1)
 	elif Input.is_action_just_pressed("down"):
 		dir = Vector2(0, 1)
+		set_flip_h(false)
+		set_rotation(-80.1)
 ###################FIMMOVIMENTACAODIRECAO###############
