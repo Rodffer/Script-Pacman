239206bf4d5f9c817d79512047ed7e6155c50714
Rayron Rodffer <44624642+Rodffer@users.noreply.github.com>
2019-03-23 22:56:16 -0300

    Add files via upload

---
+++ Pacman.gd
@@ -0,0 +1,59 @@
+extends AnimatedSprite
+
+onready var tiles = get_parent().get_node("Board/TileMap")
+
+var posdisc
+var dir
+var destdisc
+var destcont
+var speed
+
+############################MAIN##############
+func _ready():
+	spawn()
+	set_process(true)
+############################FIM DA MAIN#######
+	
+#############INICIO RESPAW####################
+func spawn() :
+	posdisc = Vector2(10, 16)
+	set_position(posdisc * 32 + Vector2(16, 16))
+	
+	dir = Vector2(0, 0)
+	destdisc = posdisc
+	destcont = get_position()
+	
+	speed = 150
+#############FIM RESPAW##################################
+###################MOVIMENTACAODIRECAO###################
+func _process(delta):
+	var delcont = destcont - get_position()
+	if delcont != Vector2(0, 0):
+		if delcont.length() < 2:
+			set_position(destdisc * 32 + Vector2(16, 16))
+			posdisc = destdisc
+		else:
+			set_position(get_position() + delta * speed * delcont.normalized())
+	else:
+		if dir != Vector2(0, 0):
+			var destdiscaux = posdisc + dir
+			if destdiscaux == Vector2(21, 10):#passar de um lado do mapa para o outro
+				destdiscaux = Vector2(0, 10)
+				set_position(Vector2(-1, 10) * 32 + Vector2(16, 16)) 
+			elif destdiscaux == Vector2(-1, 10):
+				destdiscaux = Vector2(20, 10)
+				set_position(Vector2(21, 10) * 32 + Vector2(16, 16))
+			if tiles.get_cell(destdiscaux.x, destdiscaux.y) != 0:
+				destdisc = destdiscaux
+				destcont = get_position() + 32 * dir
+	
+	dir = Vector2(0, 0)
+	if Input.is_action_pressed("left"):
+		dir = Vector2(-1, 0)
+	elif Input.is_action_just_pressed("right"):
+		dir = Vector2(1, 0)
+	elif Input.is_action_just_pressed("up"):
+		dir = Vector2(0, -1)
+	elif Input.is_action_just_pressed("down"):
+		dir = Vector2(0, 1)
+###################FIMMOVIMENTACAODIRECAO###############
